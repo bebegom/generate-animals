a5820d1398094e470ec5c4a3d28553531bb303e2
Elin Ahlgren <elin.ahlgren@hotmail.com>
2022-08-26 13:49:10 +0200

    changing indentation to tabs

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // added what server to use
-  server: {
-    port: 3000
-  }
+	plugins: [react()],
+	// added what server to use
+	server: {
+		port: 3000
+	}
 })
