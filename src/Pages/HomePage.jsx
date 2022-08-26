559f79c772dfe28253003e5e2b90ab5b6728e4d2
Elin Ahlgren <elin.ahlgren@hotmail.com>
2022-08-26 20:24:54 +0200

    you can now generate animals!

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -5,23 +5,58 @@ import { useEffect } from 'react'
 
 const HomePage = () => {
     const [amount, setAmount] = useState()
+    const [slicedAnimals, setSlicedAnimals] = useState()
+
+    const handleOnChange = () => {
+        setAmount(quantity.value)
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log('e prevented')
-        setAmount(quantity.value)
+
+        shuffle(data)
+        // console.log(data.slice(0, amount))
+        setSlicedAnimals(data.slice(0, amount))
+    }
+
+    const shuffle = (arr) => {
+        for (let i = arr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1))
+            const temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+        }
     }
     
     useEffect(() => {
-        console.log(amount)
-    }, [amount])
+        if (!slicedAnimals) {
+            return
+        }
+        // console.log(amount)
+        // shuffle(data)
+        console.log(slicedAnimals)
+
+    }, [slicedAnimals])
 
     return (
         <Container className='my-5 d-flex flex-column align-items-center'>
             <Button onClick={handleSubmit} type='submit' variant='secondary' className='my-3'>Generate</Button>
             <Form>
                 <label htmlFor="quantity" className='mx-2'>Choose how many animals you want:</label>
-                <input type="number" id="quantity" name="quantity" min="1" max={data.length} />
+                <input onChange={handleOnChange} type="number" id="quantity" name="quantity" min="1" max={data.length} />
             </Form>
+
+            <div>
+                {slicedAnimals && (
+                    <div className='my-3'>
+                        {slicedAnimals.map((animal, index) => (
+                            <div key={index}>
+                                {animal}
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
         </Container>
     )
 }
