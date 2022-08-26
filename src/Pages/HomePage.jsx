0b94aefe276fb5269d8dfdf198e4ab12b5cef61d
Elin Ahlgren <elin.ahlgren@hotmail.com>
2022-08-26 19:29:35 +0200

    created a state for the amount of animals to generate

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,18 +1,26 @@
 import {Button, Container, Form} from 'react-bootstrap'
 import data from '../../data/animals.json'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 const HomePage = () => {
+    const [amount, setAmount] = useState()
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log('e prevented')
-        console.log(data)
+        setAmount(quantity.value)
     }
+    
+    useEffect(() => {
+        console.log(amount)
+    }, [amount])
+
     return (
         <Container className='my-5 d-flex flex-column align-items-center'>
             <Button onClick={handleSubmit} type='submit' variant='secondary' className='my-3'>Generate</Button>
             <Form>
                 <label htmlFor="quantity" className='mx-2'>Choose how many animals you want:</label>
-                <input type="number" id="quantity" name="quantity" min="1" max="224" /> {/* TODO: change to data.length */}
+                <input type="number" id="quantity" name="quantity" min="1" max={data.length} />
             </Form>
         </Container>
     )
